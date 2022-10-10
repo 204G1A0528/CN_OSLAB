2a7131d046a125bffc57729ae8cab520e96e6c9f
204G1A0528 <94513439+204G1A0528@users.noreply.github.com>
2022-10-10 16:06:00 +0530

    created SJF.c

---
+++ OSLAB/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B Divya sree
+   Roll No: 204G1A0528
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 03-10-2022
+   Date of Execution: 03-10-2022
+*/
