7f2886d82c105e96dbf061b4aa17e2bf679fbe50
204G1A0528 <94513439+204G1A0528@users.noreply.github.com>
2022-11-01 12:55:40 +0530

    created LRU.c

---
+++ OSLAB/LRU.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Divya sree
+   Roll No: 204G1A0528
+   Title of the Experiment: LRU page replacement Algorithm
+   Date of Creation: 01-11-2022
+   Date of Execution: 01-11-2022
+*/
