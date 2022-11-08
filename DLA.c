3aa05844ea969935a775e43950360eeb5d52ccdc
204G1A0528 <94513439+204G1A0528@users.noreply.github.com>
2022-11-08 12:33:51 +0530

    Created DLA.c

---
+++ DLA.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Divya sree
+   Roll No: 204G1A0528
+   Title of the Experiment: DEAD LOCK AVOIDENCE BANKERS Algorithm
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
